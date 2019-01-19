b0c3415d8e3f6ad5a88feafc30fdc681464b435d
Joan Heredia Rius <watxaut@gmail.com>
2019-01-20 00:14:35 +0100

    - change allowed hosts

--- ownit/settings.py
+++ ownit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = secret.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.ownit-barcelona.es']
 
 # Application definition
 
